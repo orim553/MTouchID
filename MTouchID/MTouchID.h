bcf1a5675848f223b7070aab9e319c88210c0afb
ori mizrachi <orim553@gmail.com>
2017-01-12 16:14:50 +0200

    Initial Commit

---
+++ MTouchID/MTouchID.h
@@ -0,0 +1,19 @@
+//
+//  MTouchID.h
+//  MTouchID
+//
+//  Created by ori mizrachi on 12/01/2017.
+//  Copyright © 2017 Ori Mizrachi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTouchID.
+FOUNDATION_EXPORT double MTouchIDVersionNumber;
+
+//! Project version string for MTouchID.
+FOUNDATION_EXPORT const unsigned char MTouchIDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTouchID/PublicHeader.h>
+
+
